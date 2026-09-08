b69e32424ecfd43fa1a97c7bbd27b77fefdfc65b
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-09-08 22:14:34 +0100

    added illustrations svg for education section

--- src/components/forms/EducationForm.jsx
+++ src/components/forms/EducationForm.jsx
@@ -2,6 +2,7 @@ import { Field, Input } from '../common/Field.jsx'
 import { Button } from '../common/Button.jsx'
 import { EntryCard } from '../common/EntryCard.jsx'
 import { blankEducation } from '../../data/defaultData.js'
+import educationIllustration from '../../assets/illustrations/education.svg'
 
 export default function EducationForm({ data, onChange }) {
   const updateEntry = (id, field, value) =>
@@ -12,6 +13,12 @@ export default function EducationForm({ data, onChange }) {
 
   return (
     <div className="space-y-4">
+      <img
+        src={educationIllustration}
+        alt=""
+        aria-hidden="true"
+        className="w-40 sm:w-44 h-auto mx-auto"
+      />
       {data.map((entry, i) => (
         <EntryCard
           key={entry.id}
